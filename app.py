619c5cd78c536a8802fa5b7a72f0c11a75b411b1
Dumplin <avghdev@gmail.com>
2021-05-01 14:45:08 -0400

    Added factorial server code

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     """Renders a sample page."""
     return render_template("index.html")
+
+@app.route("/factorial/<number>")
+def factorial(number):
+    fact = 1
+    for i in range(1, int(number)+1):
+        fact = fact * i
+    return str(fact)
 
 if __name__ == '__main__':
     import os
